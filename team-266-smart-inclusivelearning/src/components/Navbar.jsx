5f45145b1f8220e63218ffc4e816738eb0ed880d
SifisoMazibuko <SifisoMazibuko@hotmail.com>
2020-09-01 00:58:55 +0200

    Creating a Navigation bar

---
+++ team-266-smart-inclusivelearning/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return(
+        <div className="nav">
+            <div className="nav__logo" /><h1>|</h1>
+            <p><b>Smart-Inclusive<br /> Learning</b></p>
+            <button className="sign-in btn btn-primary">Sign In</button>  
+        </div>
+    )
+};
