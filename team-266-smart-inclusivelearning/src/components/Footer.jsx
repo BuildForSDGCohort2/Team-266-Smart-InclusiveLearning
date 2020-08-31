7eb7532911fb8788f45aae2fecc3f3a0462d55c1
SifisoMazibuko <SifisoMazibuko@hotmail.com>
2020-09-01 00:59:31 +0200

    Creating a Footer component

---
+++ team-266-smart-inclusivelearning/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+            <p style={{ backgroundColor: '#282c34' }}>
+                &copy; Smart-Inclusive Learning Ltd. All Rights Reserved.
+            </p>
+        </footer>
+    )
+}
