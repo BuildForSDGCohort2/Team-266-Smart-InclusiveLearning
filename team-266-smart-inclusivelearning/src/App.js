87817410532795ce14028f0f6ae7d466e0913178
SifisoMazibuko <SifisoMazibuko@hotmail.com>
2020-09-01 23:35:55 +0200

    Fixing the App file and some css

--- team-266-smart-inclusivelearning/src/App.js
+++ team-266-smart-inclusivelearning/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import settings from './settings.svg';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './App.css';
@@ -12,18 +12,10 @@ function App() {
     <div className="App">
       <Navbar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={settings} className="App-logo" alt="settings" />
         <p>
           Site Under Construction please be on the look out for Updates.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <Footer/>
     </div>
