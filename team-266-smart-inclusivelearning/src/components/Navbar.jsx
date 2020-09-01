3addf0f02e797b66913482f0b0b5686ede5d3bc5
SifisoMazibuko <SifisoMazibuko@hotmail.com>
2020-09-01 23:34:17 +0200

    Adding the logo on the navbar

--- team-266-smart-inclusivelearning/src/components/Navbar.jsx
+++ team-266-smart-inclusivelearning/src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Navbar() {
     return(
         <div className="nav">
-            <div className="nav__logo" /><h1>|</h1>
-            <p><b>Smart-Inclusive<br /> Learning</b></p>
+            <div className="nav__logo" />
             <button className="sign-in btn btn-primary">Sign In</button>  
         </div>
     )
