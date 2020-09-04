146958f40cda72fddb89400686a351805a7c24ba
SifisoMazibuko <SifisoMazibuko@hotmail.com>
2020-09-04 10:56:07 +0200

    Adding popup signup feature

--- team-266-smart-inclusivelearning/src/App.js
+++ team-266-smart-inclusivelearning/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import settings from './settings.svg';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
